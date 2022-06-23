a6101f1f84a99aa2083ecc97e9a53f13e653a37b
ga73bat <ga73bat@mytum.de>
2022-06-23 21:14:36 +0200

    Dockerfile + app.py

---
+++ app_cap.py
@@ -0,0 +1,12 @@
+    # save this as app.py
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+if __name__ == "__main__":
+    app.run()
