eaffd2f65d027358c4a6a7ec01241a2aca53b1da
ga73bat <ga73bat@mytum.de>
2022-09-11 15:02:22 +0200

    flaskapp adations

--- app_cap.py
+++ app_cap.py
@@ -1,11 +1,12 @@
     # save this as app.py
-from flask import Flask, escape, request
+from flask import Flask, request
+#from flask import Flask, escape, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #name = request.args.get("name", "World")
+    name = request.args.get("name", "World")
     #return f'Hello, {escape(name)}!'
     return '<h1>Hello from Flask & Docker</h2>'
 
