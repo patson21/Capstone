2b1d87f543341ecab59d029dd7d853a980c6164e
ga73bat <ga73bat@mytum.de>
2022-09-11 14:56:21 +0200

    flaskapp adations

--- app_cap.py
+++ app_cap.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    #name = request.args.get("name", "World")
+    #return f'Hello, {escape(name)}!'
+    return '<h1>Hello from Flask & Docker</h2>'
 
 if __name__ == "__main__":
     #app.run()
     app.run(host='0.0.0.0', port=8080, debug=True)
+else: 
+    print('The app did not start')
